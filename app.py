4aecac72efb174d29fae33380a605c5007dd6bd1
Vaishali Tomar <tomarvaishali2000@gmail.com>
2026-04-22 15:51:13 +0530

    Successfully Added Frontend UI

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Student Profile App 🚀"
+    return """
+    <h1>Hello Student Profile App!</h1> 
+    <p>Name : Vaishali Tomar</p>
+    <p>Class : CS-6C</p>
+    """
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
